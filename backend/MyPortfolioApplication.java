4a78d81d08c7e61bf16d5032367346f3662312b9
Sebastian Montoya <sebastianmontoya@Caterpie.local>
2025-10-11 22:38:33 +1100

    added a dependency file

---
+++ backend/MyPortfolioApplication.java
@@ -0,0 +1,8 @@
+package backend;
+
+@SpringBootApplication
+public class MyPortfolioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyPortfolioApplication.class, args);
+    }
+}
