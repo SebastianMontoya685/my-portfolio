b9d2c0747314656130af1f7c99a934f529a0bd7d
Sebastian Montoya <sebastianmontoya@Caterpie.local>
2025-10-11 15:43:32 +1100

    created the basic layout and structure for the portfolio's homepage

---
+++ my-react-app/src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Socials from './Socials.tsx';
+import './Footer.css';
+import Links from './Links.tsx';
+
+export default function Footer() {
+    return (
+        <div className='footer'>
+            <Socials />
+            <Links />
+        </div>
+    );
+}
---
+++ my-react-app/src/components/ProjectSection/ProjectNavigation.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './ProjectNavigation.css';
+export default function ProjectNavigation() {
+    return (
+        <div className='project-navigation'>
+            Project navigation
+        </div>
+    );
+}
