259c7a47a948ef82592dfbba22b0f02aec8b3e06
Sebastian Montoya <sebastianmontoya@Caterpie.local>
2025-10-14 01:10:22 +1100

    added the initial buttons to the project section navigation. These buttons will each render a particular project that I have created when a user clicks on the button

--- my-react-app/src/components/ProjectSection/ProjectNavigation.tsx
+++ my-react-app/src/components/ProjectSection/ProjectNavigation.tsx
@@ -3,7 +3,11 @@ import './ProjectNavigation.css';
 export default function ProjectNavigation() {
     return (
         <div className='project-navigation'>
-            Project navigation
+            <button>Button 1</button>
+            <button>Button 1</button>
+            <button>Button 1</button>
+            <button>Button 1</button>
+            <button>Button 1</button>
         </div>
     );
 }
