40436315d5090fb63ab9146b374ac691709c9c17
Sebastian Montoya <sebastianmontoya@Caterpie.local>
2025-10-14 01:05:16 +1100

    refined the stylistic properties of the link tags in the right hand side section of the header section

--- my-react-app/src/components/Home/Header.tsx
+++ my-react-app/src/components/Home/Header.tsx
@@ -7,16 +7,16 @@ export default function Header() {
             <div className='header'>
                 {/* <div className='header-left-container'> */}
                 <div className='header-left'>
-                    <h1 className='header-title'>Sebastian Montoya</h1>
+                    <h1 className='header-title'>#! Sebastian Montoya</h1>
                     <p>Backend, Frontend, and AI Developer</p>
                     <button className='header-button'>See Projects</button>
                 </div>
                 {/* </div> */}
                 <div className='header-right-container'>
                     <div className='header-right'>
-                        <a href='#header'>Header</a>
-                        <a href='#projects'>Projects</a>
-                        <a href='#footer'>Footer</a>
+                        <a href='#header'><em>Header</em></a>
+                        <a href='#projects'><em>Projects</em></a>
+                        <a href='#footer'><em>Footer</em></a>
                     </div>
                 </div>
             </div>
