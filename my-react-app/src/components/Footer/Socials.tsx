63114f65d38c431087cad87ba2a30e11f7ada9cf
SebastianMontoya685 <sebastian.m@jpi.global>
2025-11-04 01:14:14 +1100

    linked more socials

--- my-react-app/src/components/Footer/Socials.tsx
+++ my-react-app/src/components/Footer/Socials.tsx
@@ -5,18 +5,33 @@ import './Socials.css';
 export default function Socials() {
     return (
         <div className="footer-socials" aria-label="right footer social links">
-            <div className="social-item">
+            <a 
+                href="https://www.instagram.com/sebastianmontoya800/" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="social-item"
+            >
                 <span className="icon-box"><FaInstagram /></span>
                 <span>Instagram.</span>
-            </div>
-            <div className="social-item">
+            </a>
+            <a 
+                href="https://www.linkedin.com/in/sebastian-montoya-232393283/" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="social-item"
+            >
                 <span className="icon-box"><FaLinkedin /></span>
                 <span>LinkedIn.</span>
-            </div>
-            <div className="social-item">
+            </a>
+            <a 
+                href="https://github.com/SebastianMontoya685" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="social-item"
+            >
                 <span className="icon-box"><FaGithub /></span>
                 <span>GitHub.</span>
-            </div>
+            </a>
             <div className="social-item">
                 <span className="icon-box"><FaFacebook /></span>
                 <span>Facebook.</span>
